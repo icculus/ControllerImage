226537dc6e7eb4bc3041816a9cd5f4403477b522
Ryan C. Gordon <icculus@icculus.org>
2026-07-18 02:27:23 -0400

    docs: Added a note about building the data files to the category documentation.

--- src/controllerimage.h
+++ src/controllerimage.h
@@ -44,6 +44,11 @@
  *   ControllerImage_CreateSurfaceForButton().
  * - Done with a gamepad? Free resources with ControllerImage_DestroyDevice().
  * - Done with the library? Call ControllerImage_Quit() to clean up.
+ *
+ * You will need a datafile, which you can build from the files that come with
+ * the library's source code. Please see README.md in the root of the source
+ * tree, or [the wiki](https://wiki.icculus.org/ControllerImage/) for the
+ * details on how to do that.
  */
 
 #ifndef INCL_CONTROLLERIMAGE_H_
